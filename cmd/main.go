b7823b1ca9c4eeda2c4bd69db0ef1b8fd453b524
jang whoemoon <palindrom615@gmail.com>
2023-11-25 15:38:23 +0900

    Migrate to aws-sdk-go-v2

--- cmd/main.go
+++ cmd/main.go
@@ -3,26 +3,32 @@ package main
 import (
 	"context"
 
+	"github.com/palindrom615/requestbin/handler"
+
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
-	"github.com/palindrom615/requestbin"
 )
 
 var tableName = "hosts"
 
 func HandleRequest(ctx context.Context, request events.LambdaFunctionURLRequest) (*events.LambdaFunctionURLResponse, error) {
-	logger, _ := requestbin.NewLogger()
-	defer logger.Sync()
-	logger.Infow("request", "request", request)
-	db := requestbin.NewDB(tableName, logger)
-	err := db.PutValue(request.RequestContext.RequestID, request.Body)
-	if err != nil {
-		return &events.LambdaFunctionURLResponse{
-			StatusCode: 500,
-			Headers:    map[string]string{"Content-Type": "text/plain"},
-			Body:       "Internal Server Error",
-		}, nil
-	}
+
+	h := handler.NewComposeHandler(
+		handler.NewEmbedInputHandler(
+			func(ctx context.Context, input interface{}) handler.CtxKey {
+				return "request"
+			},
+		),
+		handler.NewLoggingHandler[interface{}](),
+	)
+	handlerCtx := context.Background()
+	inputChan := make(chan interface{})
+	h.Handle(handlerCtx, inputChan)
+
+	go func() {
+		inputChan <- &request
+	}()
+
 	if request.RequestContext.HTTP.Method != "POST" {
 		return &events.LambdaFunctionURLResponse{
 			StatusCode: 405,
