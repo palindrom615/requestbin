82f5155f8b1ffef580ee99cdc2ab39e825368f73
jang whoemoon <palindrom615@gmail.com>
2023-11-26 00:55:45 +0900

    Implement FilterHandler

---
+++ Handler/filter.go
@@ -0,0 +1,36 @@
+package handler
+
+import (
+	"context"
+	"errors"
+)
+
+var ErrFiltered = errors.New("filtered")
+
+type FilteringHandler[I any] struct {
+	isOkay func(ctx context.Context, input I) bool
+}
+
+func NewFilteringHandler[I any](isOkay func(ctx context.Context, input I) bool) *FilteringHandler[I] {
+	return &FilteringHandler[I]{
+		isOkay: isOkay,
+	}
+}
+
+func (f *FilteringHandler[I]) Handle(ctx context.Context, input <-chan I) (context.Context, <-chan I) {
+	select {
+	case <-ctx.Done():
+		return ctx, input
+	case i := <-input:
+		newCtx, cancel := context.WithCancelCause(ctx)
+		o := make(chan I)
+		go func() {
+			defer close(o)
+			o <- i
+		}()
+		if !f.isOkay(ctx, i) {
+			cancel(ErrFiltered)
+		}
+		return newCtx, o
+	}
+}
---
+++ Handler/filter_test.go
@@ -0,0 +1,59 @@
+package handler_test
+
+import (
+	"context"
+	"testing"
+
+	"github.com/palindrom615/requestbin/handler"
+)
+
+func isOkay(ctx context.Context, input string) bool {
+	return input == "okay"
+}
+func TestFilteringHandler_HandleOkay(t *testing.T) {
+	// arrange
+	handler := handler.NewFilteringHandler(isOkay)
+	i := make(chan string)
+
+	go func() {
+		i <- "okay"
+	}()
+
+	// act
+	newCtx, o := handler.Handle(context.Background(), i)
+
+	// assert
+	select {
+	case s := <-o:
+		t.Logf("Handler returned %s", s)
+		if s != "okay" {
+			t.Errorf("filtering handler should return \"okay\", got \"%s\"", s)
+		}
+	case <-newCtx.Done():
+		t.Errorf("filtering handler should not be canceled: %s", newCtx.Err())
+	}
+}
+
+func TestFilteringHandler_HandleNotokay(t *testing.T) {
+	// arrange
+	h := handler.NewFilteringHandler(isOkay)
+	i := make(chan string)
+
+	go func() {
+		i <- "notOkay"
+	}()
+
+	// act
+	newCtx, o := h.Handle(context.Background(), i)
+
+	// assert
+	<-newCtx.Done()
+	if context.Cause(newCtx) != handler.ErrFiltered {
+		t.Errorf("FilteringHandler should be canceled because of handler.ErrFiltered, got %s", newCtx.Err())
+	}
+	s := <-o
+	t.Logf("Handler returned %s", s)
+	if s != "notOkay" {
+		t.Errorf("FilteringHandler should return \"notOkay\", got \"%s\"", s)
+	}
+}
